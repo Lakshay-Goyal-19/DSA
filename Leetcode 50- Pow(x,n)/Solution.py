2a9f7e49a87181aa5dfbe4a2deac72dbc7c45173
Lakshay-Goyal-19 <gyllakshay@gmail.com>
2025-09-29 10:31:14 +0530

    added

---
+++ Leetcode 50- Pow(x,n)/Solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        #for small value of n this is working
+        #complexity- log(n) which is too slow
+        # ans = 1
+        # if(n<0):
+        #     x=1/x
+        #     n=-n
+        # while(n>0):
+        #     ans=ans*x
+        #     n-=1
+        # return ans
+
+        #for big value of n Exponentiation by squaring method should be used
+        #complexity- log(n)
+        ans = 1
+        if(n < 0):
+            x = 1 / x
+            n = -n
+        while(n > 0):
+            if(n % 2 == 1):
+                ans *= x
+            x *= x
+            n = n//2
+        return ans
+
+            
+
+        
+
+        
